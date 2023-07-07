2703c4f308ff7927ba0cc9697dccd56d5b9ad117
rchirath <rchirath@caresystemsinc.com>
2023-07-07 20:22:23 +0530

    Add build script

---
+++ build.bat
@@ -0,0 +1,13 @@
+echo Compiling Source Files... 
+
+echo Compiling Chapter 1 source files...
+clang chap1/bank.c -o builds/chap1_bank.exe
+clang chap1/circle.c -o builds/chap1_circle.exe
+clang chap1/money.c -o builds/chap1_money.exe
+clang chap1/poly2.c -o builds/chap1_poly2.exe
+clang chap1/polynomial.c -o builds/chap1_polynomial.exe
+clang chap1/stars.c -o builds/chap1_stars.exe
+clang chap1/tax.c -o builds/chap1_tax.exe
+echo Done compiling Chapter 1 source files.
+
+pause 
