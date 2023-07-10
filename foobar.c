d270000f950a915893380d75eb13f8bd2a2dd8a3
thenlpstudent <98467332+thenlpstudent@users.noreply.github.com>
2023-07-10 09:06:16 +0530

    minified foobar

--- foobar.c
+++ foobar.c
@@ -2,32 +2,31 @@
 #include <stdio.h>
 #define MAX_SIZE 200 
 
-int say(int n, int d, char * what_to_say)
+int say(int n, int d, char * str)
 {
   if (n % d != 0)
     return 0;
-  printf("%s", what_to_say);
+  printf("%s", str);
   return 1;
 }
 
 int main(void)
 {
-
   int keys[] = {3,4,5,6,7};
   char * strs[] = {"Foo", "Bar","Zoo", "Goo", "Loo"};
   int size = 5; 
   
-  for (int i = 1; i <= 200; i++)
+  for (int i = 1; i <= MAX_SIZE; i++)
     {
       int success = 0; 
       for (int j = 0; j < size; j++)
 	{
-	  int r = say(i, keys[j], strs[j]);
-	  success = success | r;
+	  success |= say(i, keys[j], strs[j]);
 	}
       if (!success)
 	printf("%d", i);
       printf("\n");
     }
-  return 0; 
+	
+  return 0; 	
 }
