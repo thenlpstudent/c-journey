c618ac1c8ce91adddf90cfbab099f8cc6d3b59d7
thenlpstudent <98467332+thenlpstudent@users.noreply.github.com>
2023-07-10 17:34:30 +0530

    format tax.c

--- chap4/tax.c
+++ chap4/tax.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float calc_tax(float income, int * conditions, float * fixed_tax,int * taxable_amounts, int * precentages, int size)
+float calc_tax(float income, int * conditions, float * fixed_tax, int * taxable_amounts, int * precentages, int size)
 {
   int ind = 0; 
   for (int i = 0; i < size; i++)
