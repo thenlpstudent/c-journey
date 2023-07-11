71212d32de043002eb3a0ca3c06db6092ca47d99
rchirath <rchirath@caresystemsinc.com>
2023-07-11 13:33:39 +0530

    foobar on struts

--- foobar.c
+++ foobar.c
@@ -1,6 +1,11 @@
-
 #include <stdio.h>
 #define MAX_SIZE 200 
+
+typedef struct Condition
+{
+  int key;
+  char * say; 
+} Condition;
 
 int say(int n, int d, char * str)
 {
@@ -10,23 +15,53 @@ int say(int n, int d, char * str)
   return 1;
 }
 
+void printConditions(Condition * conditions, int size)
+{
+  for (int i = 0; i < size; i++)
+    {
+      printf("Key: %d \tSay: %s\n", conditions[i].key, conditions[i].say);
+    }
+}
+
 int main(void)
 {
-  int keys[] = {3,4,5,6,7};
-  char * strs[] = {"Foo", "Bar","Zoo", "Goo", "Loo"};
-  int size = 5; 
+  Condition conditions [] =
+    {
+     {
+      .key = 3,
+      .say = "Foo"
+     },
+     {
+      .key = 4,
+      .say = "Bar"
+     },
+     {
+      .key = 5,
+      .say = "Zoo"
+     },
+     {
+      .key = 6,
+      .say = "Loo"
+     },
+     {
+      .key = 7,
+      .say = "Woo"
+     },
+    };
   
+  int size = 5;
+    
   for (int i = 1; i <= MAX_SIZE; i++)
     {
       int success = 0; 
       for (int j = 0; j < size; j++)
 	{
-	  success |= say(i, keys[j], strs[j]);
+	  success |= say(i, conditions[j].key, conditions[j].say);
 	}
       if (!success)
 	printf("%d", i);
       printf("\n");
     }
-	
+  	
   return 0; 	
 }
